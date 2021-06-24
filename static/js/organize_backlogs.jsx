941a1629ffacec4346057d64c774b36b88aec142
Amber Orrange <orrangeamber@gmail.com>
2021-06-24 22:01:14 +0000

    filter by genre

--- static/js/organize_backlogs.jsx
+++ static/js/organize_backlogs.jsx
@@ -1,8 +1,5 @@
 "use strict";
 
-
-
-
 function BacklogEntry(props) {
   return (
     <div className="backlog_entry">
@@ -10,25 +7,37 @@ function BacklogEntry(props) {
       <img  className="backlog_image" src={props.image} /> <br></br>
       Genre: {props.genre} <br></br>
       Ownership Status: {props.ownership_status}<br></br>
-      Play Status: {props.play_status}<br></br>
+      Play Status: {props.play_status ? 'Currently playing' : 'Not playing'}<br></br>
       Playing On: {props.platform}<br></br>
     </div>
   );
 }
 
+function filterOption(props) {
+  return (
+    <option value="{props.name}">{props.name}</option>
+  )
+
+}
+
 function BacklogContainer() {
   const [backlogs, updateBacklogs] = React.useState([]);
+  const [displayedBacklogs, updateDisplayedBacklogs] = React.useState([]);
+  const [genreFilter, updateGenreFilter] = React.useState(undefined); // ex.: 'fantasy', 'RPG'
 
   React.useEffect(() => {
     fetch("/api/backlog")
       .then((response) => response.json())
-      .then((data) => updateBacklogs(data));
+      .then((data) => {
+        // Load initial backlog data into component
+        updateBacklogs(data);
+        updateDisplayedBacklogs(data);
+      });
   }, []);
 
   const backlogEntries = [];
-  console.log(backlogs)
-
-  for (const backlog of backlogs) {
+  
+  for (const backlog of displayedBacklogs) {
     backlogEntries.push(
       <BacklogEntry
         key={backlog.backlog_id}
@@ -42,7 +51,16 @@ function BacklogContainer() {
     );
   }
 
-  return <div>{backlogEntries}</div>;
+  const genres = [];
+  for (const backlog of backlogs) {
+    <filterOption
+      key={backlog.genre}
+      name={backlog.genre}
+      />
+  } 
+
+
+  return <div>{backlogEntries}  </div>;
 }
 
 ReactDOM.render(<BacklogContainer />, document.getElementById("container"));
