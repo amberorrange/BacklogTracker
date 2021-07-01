7f9b14dc0e625cd5c02aad3ce7ddc5ab3f4b70ce
Amber Orrange <orrangeamber@gmail.com>
2021-07-01 07:30:43 +0000

    styling login/create account

--- static/js/project.js
+++ static/js/project.js
@@ -14,3 +14,15 @@ function togglePasswordVisibilty() {
 }
 
 
+//Reminds user of password requirements
+function passwordRequirements() {
+    const pw = getElementById('pw')
+    const pwconfirm = getElementById('pwconfirm')
+
+    if (pw.value.length >= 8 && pwconfirm.value.length >= 8) {
+        $('#passwordHelpBack').css('color', 'green');
+        // document.getElementById('passwordHelpBlock').style.color = 'green';   
+    } else {
+        document.getElementById('passwordHelpBlock').style.color = 'red';  
+    }
+}
